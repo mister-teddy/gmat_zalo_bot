dc33a306c5177d300a5920cae9e93af97854fd68
PhatNH4 <phatnh4@vng.com.vn>
2025-06-20 14:27:24 +0700

    init project

---
+++ target/debug/deps/gmat_zalo_bot-47d896242105f7b6.d
@@ -0,0 +1,5 @@
+/Users/teddy/Desktop/github/gmat_zalo_bot/target/debug/deps/libgmat_zalo_bot-47d896242105f7b6.rmeta: src/main.rs
+
+/Users/teddy/Desktop/github/gmat_zalo_bot/target/debug/deps/gmat_zalo_bot-47d896242105f7b6.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/gmat_zalo_bot-efe5234a6d2fe7ea.d
@@ -0,0 +1,5 @@
+/Users/teddy/Desktop/github/gmat_zalo_bot/target/debug/deps/libgmat_zalo_bot-efe5234a6d2fe7ea.rmeta: src/main.rs
+
+/Users/teddy/Desktop/github/gmat_zalo_bot/target/debug/deps/gmat_zalo_bot-efe5234a6d2fe7ea.d: src/main.rs
+
+src/main.rs:
